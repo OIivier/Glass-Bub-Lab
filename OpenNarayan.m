360d60c4e4e27441c1738632e587379170828daf
vincentmackay <vince.mackay@gmail.com>
2018-05-14 10:27:52 -0400

    Delete OpenNarayan.m

--- OpenNarayan.m
+++
@@ -1,9 +0,0 @@
-function DataNarayan = OpenNarayan(CaseNumber)
-    cd('/home/vincent/Documents/Research Thesis/Data');
-    fileName = ['Case ',num2str(CaseNumber), ' QRS Subtracted Basket Data.txt'];
-    fileID = fopen(fileName,'r');
-    formatSpec = '%f';
-    DataNarayan = fscanf(fileID,formatSpec);
-    NFrames = 4000; % number of frames
-    Asz = size(DataNarayan,1);
-    DataNarayan = reshape(DataNarayan,NFrames,Asz/NFrames);
