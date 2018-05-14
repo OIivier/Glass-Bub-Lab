5f62c6af03fe5520b6bf5390697a81d1f16c11d6
vincentmackay <vince.mackay@gmail.com>
2018-05-14 10:36:34 -0400

    Delete rotoridenttest.m

--- rotoridenttest.m
+++
@@ -1,8 +0,0 @@
-%S = load('C:\Users\Olivier\Documents\MATLAB\Simulationsfolder\Simul2_0-0-15_0-0-0_0-0-4.mat');
-%V = load('C:\Users\Olivier\Documents\MATLAB\Simulationsfolder\Simul3_0-0-15_0-0-0_0-0-4.mat');
-W = load('/home/vincent/Documents/McGill/PHYS 459 - Research Thesis/Simulations/Vincent codes/Sim_18-Jan-2018_D1_v20_R1.mat');
-
-%data2 = S.m;
-%data3 = V.m;
-data1 = W.m;
-disp(rotorident(data1));
