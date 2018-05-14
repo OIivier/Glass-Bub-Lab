ea6af9459df44b5440d47a60924b99d70beb7633
vincentmackay <vince.mackay@gmail.com>
2018-05-14 10:24:18 -0400

    Delete GBRColorMap.m

--- GBRColorMap.m
+++
@@ -1,8 +0,0 @@
-function GBRColorMap = GBRColorMap(ColorMapResolution)
-
-GBRColorMapTop = flipud([zeros(1,ColorMapResolution);2.^[(1.0/ColorMapResolution):(1.0/ColorMapResolution):1]-2^(1.0/ColorMapResolution);zeros(1,ColorMapResolution)]');
-GBRColorMapBottom = [2.^[(1.0/ColorMapResolution):(1.0/ColorMapResolution):1]-2^(1.0/ColorMapResolution);zeros(1,ColorMapResolution);zeros(1,ColorMapResolution)]';
-GBRColorMapTop = flipud([zeros(1,ColorMapResolution);[(1.0/ColorMapResolution):(1.0/ColorMapResolution):1];zeros(1,ColorMapResolution)]');
-GBRColorMapBottom = [[(1.0/ColorMapResolution):(1.0/ColorMapResolution):1];zeros(1,ColorMapResolution);zeros(1,ColorMapResolution)]';
-
-GBRColorMap = [GBRColorMapTop;GBRColorMapBottom];
